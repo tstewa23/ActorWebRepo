fac8c1840a2a8fdbe712d4c7ae87d0837d351a9d
taylor stewart <stewarttaylor234@gmail.com>
2025-03-20 22:43:49 -0600

    sumbit on entryInput now POSTs data to API

--- src/components/entryInput.jsx
+++ src/components/entryInput.jsx
@@ -1,25 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const EntryInput = () => {
+    const [name, setName] = useState('');
+    const [actor, setActor] = useState('');
+    const [connection, setConnection] = useState('');
+
+    const data = {
+        name: name,
+        actor: actor,
+        connection: connection,
+    };
+
+    const sendEntry = async () => {
+        try {
+            const response = await fetch('https://my-express-api-tstewa23.onrender.com/submit', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            })
+            if (response.ok) {
+                console.log('data sent')
+                setName('');
+                setActor('');
+                setConnection('');
+            } else {
+                console.error('Response Error:', response.status)
+            }
+        } catch (error) {
+            console.error('Error with request:', error)
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        sendEntry();
+    };
+
+
     return (
         <>
             <h2>EntryInput</h2>
-            <form id="dataForm">
+            <form id="dataForm" onSubmit={handleSubmit}>
                 <input
                     style={{ backgroundColor: 'white', color: 'black' }}
-                    type="text" id="name"
+                    type="text"
+                    value={name}
+                    onChange={(e) => {
+                        setName(e.target.value)
+                    }}
+                    id="name"
                     placeholder="Enter your name"
                     required
                 />
                 <input
                     style={{ backgroundColor: 'white', color: 'black' }}
-                    type="text" id="actor"
+                    type="text"
+                    value={actor}
+                    onChange={(e) => setActor(e.target.value)}
+                    id="actor"
                     placeholder="Enter your Actor"
                     required
                 />
                 <input
                     style={{ backgroundColor: 'white', color: 'black' }}
-                    type="text" id="connection"
+                    type="text"
+                    value={connection}
+                    onChange={(e) => setConnection(e.target.value)}
+                    id="connection"
                     placeholder="Enter the connection"
                     required
                 />
