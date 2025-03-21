52dee33a80efabfb0fd9852c41b9dbdd438d85bb
taylor stewart <stewarttaylor234@gmail.com>
2025-03-20 23:43:16 -0600

    entryLog GET's data for table and websocket.io checks for emitted events

--- src/components/entryLog.jsx
+++ src/components/entryLog.jsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { io } from 'socket.io-client';
 
 const EntryLog = () => {
+    const [data, setData] = useState([])
+
+    async function fetchData() {
+        try {
+            const response = await fetch('https://my-express-api-tstewa23.onrender.com/data');
+            const data = await response.json();
+            setData(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    useEffect(() => {
+        const socket = io("https://my-express-api-tstewa23.onrender.com")
+        socket.on("connect", () => {
+            fetchData();
+        });
+        socket.on("newEntry", () => {
+            fetchData();
+        });
+        socket.on("newDelete", () => {
+            fetchData();
+        });
+        return () => socket.disconnect();
+    }, []);
+
     return (
         <>
             <h2>Entry Log</h2>
@@ -13,6 +40,13 @@ const EntryLog = () => {
                     </tr>
                 </thead>
                 <tbody id="dataTable">
+                    {data.length === 0 ? (
+                        <tr><td colSpan="3">No data available</td></tr>
+                    ) : data.map(item => (
+                        <tr>
+                            <td>{item.name}</td><td>{item.actor}</td><td>{item.connection}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </>
