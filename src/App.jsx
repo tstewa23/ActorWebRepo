082ca199e6b4ebc96f44d9537e4095c8e19c23b7
taylor stewart <stewarttaylor234@gmail.com>
2025-03-21 00:28:58 -0600

    tests bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Welcome from './rWelcome'
 import ActorWeb from './rActorWeb'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/actorWeb" element={<ActorWeb />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Welcome />} />
+    //     <Route path="/actorWeb" element={<ActorWeb />} />
+    //   </Routes>
+    // </Router>
+    <h1>hi</h1>
   )
 }
 
