8d5d8de9db19f6e9b8aa0007a7bbd3a1b2eb976c
taylor stewart <stewarttaylor234@gmail.com>
2025-03-20 22:57:51 -0600

    entryReset DELETEs data in API

--- src/components/entryReset.jsx
+++ src/components/entryReset.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
 const EntryReset = () => {
+    const resetDB = async () => {
+
+        try {
+            const response = await fetch('https://my-express-api-tstewa23.onrender.com/delete', {
+                method: 'DELETE',
+            });
+
+            const result = await response.json();
+
+            console.log(result.message)
+        }
+        catch (error) {
+            console.error("DELETE error:", error)
+        }
+
+    }
+
     return (
         <>
             <p id="responseMessage"></p>
-            <button id="myButton">reset</button>
+            <button onClick={resetDB} id="myButton">reset</button>
         </>
     )
 }
